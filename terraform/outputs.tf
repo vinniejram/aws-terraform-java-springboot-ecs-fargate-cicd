4c14acf508698e3641207e9004787929d7c8af80
vinniejram <vinod.jothiram@gmail.com>
2022-11-03 16:19:37 -0600

    test eventbridge.tf plan

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,25 +5,3 @@ output "alb_hostname" {
 output "vpc_subnets" {
   value = [aws_subnet.public.*.cidr_block]
 }
-
-
-output "eventbridge_bus_arn" {
-  description = "The EventBridge Bus ARN"
-  value       = module.eventbridge.eventbridge_bus_arn
-}
-
-# EventBridge Connection
-output "eventbridge_connection_ids" {
-  description = "The EventBridge Connection IDs created"
-  value       = module.eventbridge.eventbridge_connection_ids
-}
-
-output "eventbridge_connection_arns" {
-  description = "The EventBridge Connection ARNs"
-  value       = module.eventbridge.eventbridge_connection_arns
-}
-
-output "eventbridge_api_destination_arns" {
-  description = "The EventBridge API Destination ARNs"
-  value       = module.eventbridge.eventbridge_api_destination_arns
-}
